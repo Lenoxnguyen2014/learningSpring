1f0acdd7b7a55a518c62872ddc62ee417c4e9690
Lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2020-07-23 07:45:12 -0700

    swipe like tinder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Deck from "./Deck";
+import "./Deck.css";
+const App = () => {
+return(
+<>
+<h1>Disney Tinder <span role="img" aria-label="heart emoji">🖤</span></h1>
+<Deck />
+</>
+)
+}
+export default App;
